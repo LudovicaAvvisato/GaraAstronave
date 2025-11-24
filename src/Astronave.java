aa92e8fac5ef7cc1adeaa97165141e6d1f622ea5
studente <test@gmail.com>
2025-11-24 12:54:06 +0100

    GaraAstronave inizio progetto

--- src/Astronave.java
+++ src/Astronave.java
@@ -3,9 +3,14 @@ import java.util.Scanner;
 public class Astronave implements Runnable{
     int id;
     GiudiceDiGara giudice;
-    public Astronave(int id,GiudiceDiGara giudice ){
+    int per;
+    int vel;
+
+    public Astronave(int id,GiudiceDiGara giudice, int per, int vel ){
         this.id=id;
         this.giudice=giudice;
+        this.per=per;
+        this.vel=vel;
     }
     
     @Override
@@ -17,5 +22,9 @@ public class Astronave implements Runnable{
 
     public void gioca(){
 
+    }
+
+    public int calcoloP(){
+        return per*vel;
     }
 }
