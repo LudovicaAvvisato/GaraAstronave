9af21112db05ed5c09795728efd5a95569345d45
studente <test@gmail.com>
2025-11-24 12:39:58 +0100

    GaraAstronave inizio progetto

--- src/Astronave.java
+++ src/Astronave.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Astronave implements Runnable{
     int id;
     GiudiceDiGara giudice;
@@ -10,10 +12,10 @@ public class Astronave implements Runnable{
     public void run(){
             System.out.println("Sono l'astronave nr: "+ this.id);
             gioca();
-            try {
-                sleep(3600);
-            } catch (InterruptedException e) {
-                System.out.println("Errore nella transizione da Running a sleeping");
-            }
+
+    }
+
+    public void gioca(){
+
     }
 }
